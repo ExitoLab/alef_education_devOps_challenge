a81d36dc0d961b8fa3eecfe9b9cdc306cd3f933f
Ige Adetokunbo Temitayo <igeadetokunbo@gmail.com>
2022-08-07 23:55:03 +0400

    feat: trying out elastic search
    Signed-off-by: Ige Adetokunbo Temitayo <igeadetokunbo@gmail.com>

--- main.go
+++ main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	routes "alef_education_devops_challenge/routes"
+	"context"
+	"encoding/json"
 	"log"
 	"os"
+	"strconv"
+	"strings"
+	"sync"
 
+	"github.com/elastic/go-elasticsearch/esapi"
 	"github.com/elastic/go-elasticsearch/v8"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.New()
+
+	var (
+		r  map[string]interface{}
+		wg sync.WaitGroup
+	)
 
 	router.Use(gin.Logger())
 	routes.TaskRouter(router)
@@ -29,7 +40,66 @@ func main() {
 	}
 
 	defer res.Body.Close()
-	log.Println(res)
+	// Check response status
+	if res.IsError() {
+		log.Fatalf("Error: %s", res.String())
+	}
+
+	// Deserialize the response into a map.
+	if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
+		log.Fatalf("Error parsing the response body: %s", err)
+	}
+	// Print client and server version numbers.
+	log.Printf("Client: %s", elasticsearch.Version)
+	log.Printf("Server: %s", r["version"].(map[string]interface{})["number"])
+	log.Println(strings.Repeat("~", 37))
+
+	// 2. Index documents concurrently
+	//
+	for i, title := range []string{"Test One", "Test Two"} {
+		wg.Add(1)
+
+		go func(i int, title string) {
+			defer wg.Done()
+
+			// Build the request body.
+			var b strings.Builder
+			b.WriteString(`{"title" : "`)
+			b.WriteString(title)
+			b.WriteString(`"}`)
+
+			// Set up the request object.
+			req := esapi.IndexRequest{
+				Index:      "test",
+				DocumentID: strconv.Itoa(i + 1),
+				Body:       strings.NewReader(b.String()),
+				Refresh:    "true",
+			}
+
+			// Perform the request with the client.
+			res, err := req.Do(context.Background(), es)
+			if err != nil {
+				log.Fatalf("Error getting response: %s", err)
+			}
+			defer res.Body.Close()
+
+			if res.IsError() {
+				log.Printf("[%s] Error indexing document ID=%d", res.Status(), i+1)
+			} else {
+				// Deserialize the response into a map.
+				var r map[string]interface{}
+				if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
+					log.Printf("Error parsing the response body: %s", err)
+				} else {
+					// Print the response status and indexed document version.
+					log.Printf("[%s] %s; version=%d", res.Status(), r["result"], int(r["_version"].(float64)))
+				}
+			}
+		}(i, title)
+	}
+	wg.Wait()
+
+	log.Println(strings.Repeat("-", 37))
 
 	port := ":" + os.Getenv("PORT")
 	router.Run(port)
